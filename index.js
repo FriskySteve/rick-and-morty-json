500d363363300522ec68b9fe2a9853bd149843be
Michal Stasiak <michal.stasiak@protonmail.com>
2025-02-02 22:36:51 +0100

    finish json version

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 const charactersContainer = document.querySelector("#characters-container");
-let page = 2;
-let jsonUrl = `http://localhost:3000/characters?_page=${page}&_per_page=5`;
-let jsonUrl2 = `http://localhost:3000/characters?_page=${page}&_per_page=5`;
+const jsonUrl = `http://localhost:3000/characters`;
+const filter = {
+  name: "",
+  status: "Alive",
+  page: 1,
+  pagination: "?_per_page=5",
+};
+const operator = {
+  first: 1,
+  last: 1,
+};
 
-function getNewCharacterData(id) {
+async function fetchCharacters() {
+  clearCharacterCards();
+  try {
+    const urlToFetch = `${jsonUrl}${filter.pagination}&name_like=${filter.name}&status=${filter.status}&_page=${filter.page}`;
+    const response = await fetch(urlToFetch);
+    const data = await response.json();
+    operator.first = data.first;
+    operator.last = data.last;
+    data.data.forEach((character) => {
+      createCharacterCards(character);
+    });
+    return data;
+  } catch (error) {
+    console.log("Cos sie syplo.. ", error);
+  }
+}
+
+function filtering() {
+  const radioBtn = document.querySelectorAll('input[type="radio"]');
+  const filterByName = document.querySelector("#api-name");
+
+  filterByName.addEventListener("keyup", (event) => {
+    filter.name = event.target.value;
+    filter.page = 1;
+    fetchCharacters();
+  });
+  radioBtn.forEach((button) => {
+    button.addEventListener("change", (event) => {
+      filter.status = event.target.value;
+      filter.page = 1;
+      fetchCharacters();
+    });
+  });
+}
+
+function getNewCharacterData() {
   const characterName = document.querySelector("#character-name");
   const characterStatus = document.querySelector("#character-status");
   const characterSpecies = document.querySelector("#character-species");
@@ -11,60 +54,28 @@ function getNewCharacterData(id) {
 
   addCharacterBtn.addEventListener("click", async (event) => {
     event.preventDefault();
+    const dataWithId = await fetch(jsonUrl);
+    const id = await dataWithId.json();
+
     try {
       const character = {
-        id: id + 1,
+        id: id.length + 1,
         name: characterName.value,
         status: characterStatus.value,
         species: characterSpecies.value,
         image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
       };
-      const response = await fetch("http://localhost:3000/characters", {
+      const response = await fetch(jsonUrl, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(character),
       });
-      const data = await response.json();
-
-      console.log(data);
     } catch (error) {
       console.log(error);
     }
   });
-}
-
-async function addCharacter(character, charactersData) {
-  try {
-    const characterData = {
-      id: charactersData.items + 1,
-      name: character.name,
-      status: character.status,
-      species: character.species,
-      image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-    };
-    const response = await fetch("http://localhost:3000/characters", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(characterData),
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function fetchCharacters(url) {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log("Cos sie syplo.. ", error);
-  }
 }
 
 function addMessage() {
@@ -73,32 +84,26 @@ function addMessage() {
     "Nie znaleziono postaci spełniających kryteria wyszukiwania.";
 }
 
-function navigate(data) {
+function navigate() {
   const btnPrev = document.querySelector("#btn-prev");
   const btnNext = document.querySelector("#btn-next");
 
-  btnPrev.addEventListener("click", async () => {
-    if (page >= data.first) {
-      page -= 1;
-      //   await main(`http://localhost:3000/characters?_page=${page}&_per_page=5`);
-      await updateCharacterCards(
-        `http://localhost:3000/characters?_page=${page}&_per_page=5`
-      );
+  btnPrev.addEventListener("click", () => {
+    if (filter.page > operator.first) {
+      filter.page -= 1;
+      fetchCharacters();
     } else {
       addMessage();
-      page = 0;
+      filter.page = 0;
     }
   });
-  btnNext.addEventListener("click", async () => {
-    if (page <= data.last) {
-      page += 1;
-      //   await main(`http://localhost:3000/characters?_page=${page}&_per_page=5`);
-      await updateCharacterCards(
-        `http://localhost:3000/characters?_page=${page}&_per_page=5`
-      );
+  btnNext.addEventListener("click", () => {
+    if (filter.page < operator.last) {
+      filter.page += 1;
+      fetchCharacters();
     } else {
       addMessage();
-      page = data.last + 1;
+      filter.page = operator.last + 1;
     }
   });
 }
@@ -107,46 +112,45 @@ function clearCharacterCards() {
   charactersContainer.innerHTML = "";
 }
 
-async function updateCharacterCards(url) {
-  clearCharacterCards();
-
-  const characterData = await fetchCharacters(url);
-  characterData.data.forEach((character) => {
-    createCharacterCards(character);
-  });
-}
-
 function createCharacterCards(characterData) {
   const container = document.createElement("div");
   const characterImg = document.createElement("img");
   const characterName = document.createElement("h1");
   const characterStatus = document.createElement("p");
   const characterSpecies = document.createElement("p");
+  const deleteBtn = document.createElement("button");
 
+  deleteBtn.textContent = "Usuń postać";
+  deleteBtn.classList.add("delete");
   characterImg.src = characterData.image;
   characterName.textContent = characterData.name;
   characterStatus.textContent = `Status: ${characterData.status}`;
   characterSpecies.textContent = `Gatunek: ${characterData.species}`;
   container.classList.add("sheet");
 
+  deleteBtn.addEventListener("click", async () => {
+    const response = await fetch(`${jsonUrl}/${characterData.id}`, {
+      method: "DELETE",
+    });
+    fetchCharacters();
+  });
+
   container.append(
     characterImg,
     characterName,
     characterStatus,
-    characterSpecies
+    characterSpecies,
+    deleteBtn
   );
   charactersContainer.appendChild(container);
 }
+async function test(id) {}
 
-async function main(url) {
-  clearCharacterCards();
-  const characterData = await fetchCharacters(url);
-  characterData.data.forEach((character) => {
-    createCharacterCards(character);
-  });
-  navigate(characterData);
-  getNewCharacterData(characterData.items);
-  //   console.log(characterData.items);
+function main() {
+  fetchCharacters();
+  filtering();
+  navigate();
+  getNewCharacterData();
 }
 
-main(jsonUrl);
+main();
